7132a46e9e8ceca166af90260b789810488d6fab
lafleurelle <marinaclever@gmail.com>
2018-11-07 14:40:03 +0300

    Update calculate_features_for_test.py

--- calculate_features_for_test.py
+++ calculate_features_for_test.py
@@ -20,19 +20,35 @@ def worker(tsobj):
 
 pbmap = OrderedDict([(0,'u'), (1,'g'), (2,'r'), (3,'i'), (4, 'z'), (5, 'Y')])
 
-def pooler(tsdict, func):
+def pooler(total_feats, tsdict, func):
     with multiprocessing.Pool() as pool:  
         results = pool.imap(func, list(tsdict.values()))
+        features_list = []
         for res in results:
             features_list.append(res)
-        del results, res, 
-    return features_list
+        featuretable = featurize.assemble_featureset(features_list=features_list,\
+                                                  time_series=tsdict_test.values())
+        total_feats = pd.concat([total_feats, featuretable], axis=0)
+        tsdict = OrderedDict()
+    return total_feats
 
 def init_list_of_objects(size):
-    list_of_objects = list()
+    t = list()
+    m = list()
+    e = list()
     for i in range(0,size):
-        list_of_objects.append([])
-    return list_of_objects
+        t.append([])
+        m.append([])
+        e.append([])
+    return t, m, e
+
+def parse_timeserie(row, num_passbands, t, m, e):
+    for pb in range(num_passbands):
+        if int(row[2])==pb:
+            t[pb].append(float(row[1]))
+            m[pb].append(float(row[3]))
+            e[pb].append(float(row[4]))
+    return t, m, e
 
 features_to_use = ["amplitude",
                    "percent_beyond_1_std",
@@ -49,71 +65,44 @@ features_to_use = ["amplitude",
 lcfilename_test = '../input/PLAsTiCC-2018/test_set.csv'
 
 
+start = time.time()
 tsdict_test = OrderedDict()
 with open(lcfilename_test, "r") as csvfile:
     datareader = csv.reader(csvfile)
     object_num = 0
     features_list = []
     rows_count=0
-    t=init_list_of_objects(6)
-    m=init_list_of_objects(6)
-    e=init_list_of_objects(6)
-    objects_list=[]
-    total_feats=pd.DataFrame()
+    t, m, e=init_list_of_objects(num_passbands)
+    total_feats = pd.DataFrame()
     for row in datareader:
         if rows_count==0:
             names=row
             rows_count+=1
         elif rows_count==1:
             object_num = row[0]
-            for pb in range(6):
-                if int(row[2])==pb:
-                    t[pb].append(float(row[1]))
-                    m[pb].append(float(row[3]))
-                    e[pb].append(float(row[4]))
+            t, m, e = parse_timeserie(row, num_passbands, t, m, e)
             rows_count+=1
         else:
             if row[0]==object_num:
+                t, m, e = parse_timeserie(row, num_passbands, t, m, e)
                 rows_count+=1
-                for pb in range(6):
-                    if int(row[2])==pb:
-                        t[pb].append(float(row[1]))
-                        m[pb].append(float(row[3]))
-                        e[pb].append(float(row[4]))
             else:
                 rows_count+=1
                 t1=[np.array(i) for i in t]
                 m1=[np.array(i) for i in m]
                 e1=[np.array(i) for i in e]
                 thisid=int(object_num)
-                objects_list.append(thisid)
                 tsdict_test[thisid] = TimeSeries(t=t1, m=m1, e=e1,name=thisid)
+                if len(tsdict_test.values())%10000==0:
+                    total_feats = pooler(total_feats, tsdict_test, worker)
                 
-                if len(tsdict_test.values())%100000==0:
-
-                    features_list=pooler(tsdict_test, worker)
-                    featuretable = featurize.assemble_featureset(features_list=features_list,\
-                                                  time_series=tsdict_test.values())
-                    total_feats=pd.concat([total_feats, featuretable], axis=0)
-                    tsdict_test=OrderedDict()
-                    features_list=[]
-                    gc.collect()
-
-                del t[:], m[:], e[:], t, m, e
-                
-                t=init_list_of_objects(6)
-                m=init_list_of_objects(6)
-                e=init_list_of_objects(6)
+                t, m, e=init_list_of_objects(num_passbands)
                 object_num = row[0]
                 
-                for pb in range(6):
-                    if int(row[2])==pb:
-                        t[pb].append(float(row[1]))
-                        m[pb].append(float(row[3]))
-                        e[pb].append(float(row[4]))
-if len(tsdict_test.values())>0:
-    features_list=pooler(tsdict_test, worker)
-    featuretable = featurize.assemble_featureset(features_list=features_list,\
-                                                  time_series=tsdict_test.values())
-    total_feats=pd.concat([total_feats, featuretable], axis=0)
+                t, m, e = parse_timeserie(row, num_passbands, t, m, e)
 
+if len(tsdict_test.values()) > 0:
+    total_feats = pooler(total_feats, tsdict_test, worker)
+
+print (time.time() - start)
+print (((total_feats!=canonized_result).astype(int)).sum(axis=1).sum(axis=0))
